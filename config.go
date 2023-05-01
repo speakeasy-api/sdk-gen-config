9d3e3b4fbed18f48bd73407cefda56debd010201
anuraagnalluri <anuraagnalluri@gmail.com>
2023-05-01 10:22:50 -0700

    feat: add config for workflow files (#6)
    * refactor: change json tag for common sdk gen config
    
    * feat: include workflow structs for writing files
    
    * add sdk flattening to gen config
    
    * add permissions
    
    * add constants to config.go for referenced keys
    
    * fix spacing
    
    * replace openapi_doc_location const w/ openapi_docs

--- config.go
+++ config.go
@@ -1,6 +1,24 @@
 package config
 
-const Version = "1.0.0"
+const (
+	Version               = "1.0.0"
+	GithubWritePermission = "write"
+
+	// Constants to be used as keys in the config files
+
+	BaseServerURL          = "baseServerUrl"
+	TelemetryEnabled       = "telemetryEnabled"
+	SDKClassName           = "sdkClassName"
+	SingleTagPerOp         = "singleTagPerOp"
+	TagNamespacingDisabled = "tagNamespacingDisabled"
+	Languages              = "languages"
+	Mode                   = "mode"
+	GithubAccessToken      = "github_access_token"
+	SpeakeasyApiKey        = "speakeasy_api_key"
+	OpenAPIDocAuthHeader   = "openapi_doc_auth_header"
+	OpenAPIDocAuthToken    = "openapi_doc_auth_token"
+	OpenAPIDocs            = "openapi_docs"
+)
 
 type Management struct {
 	DocChecksum       string `yaml:"docChecksum"`
@@ -15,12 +33,8 @@ type Comments struct {
 }
 
 type Generation struct {
-	BaseServerURL          string    `yaml:"baseServerUrl,omitempty"`
-	Comments               *Comments `yaml:"comments,omitempty"`
-	TelemetryEnabled       bool      `yaml:"telemetryEnabled"`
-	SDKClassName           string    `yaml:"sdkClassName"`
-	TagNamespacingDisabled bool      `yaml:"tagNamespacingDisabled,omitempty"`
-	SingleTagPerOp         bool      `yaml:"singleTagPerOp"`
+	CommentFields map[string]bool `yaml:"comments,omitempty"`
+	Fields        map[string]any  `yaml:",inline"`
 }
 
 type LanguageConfig struct {
@@ -34,6 +48,8 @@ type SdkGenConfigField struct {
 	RequiredForPublishing *bool   `yaml:"requiredForPublishing,omitempty" json:"required_for_publishing,omitempty"`
 	DefaultValue          *any    `yaml:"defaultValue,omitempty" json:"default_value,omitempty"`
 	Description           *string `yaml:"description,omitempty" json:"description,omitempty"`
+	Language              *string `yaml:"language,omitempty" json:"language,omitempty"`
+	SecretName            *string `yaml:"secretName,omitempty" json:"secret_name,omitempty"`
 	ValidationRegex       *string `yaml:"validationRegex,omitempty" json:"validation_regex,omitempty"`
 	ValidationMessage     *string `yaml:"validationMessage,omitempty" json:"validation_message,omitempty"`
 }
@@ -45,17 +61,82 @@ type Config struct {
 	Languages     map[string]LanguageConfig `yaml:",inline"`
 }
 
+type PublishWorkflow struct {
+	Name string    `yaml:"name"`
+	On   PublishOn `yaml:"on"`
+	Jobs Jobs      `yaml:"jobs"`
+}
+
+type PublishOn struct {
+	Push Push `yaml:"push"`
+}
+
+type Push struct {
+	Branches []string `yaml:"branches"`
+	Paths    []string `yaml:"paths"`
+}
+
+type GenerateWorkflow struct {
+	Name        string      `yaml:"name"`
+	Permissions Permissions `yaml:"permissions"`
+	On          GenerateOn  `yaml:"on"`
+	Jobs        Jobs        `yaml:"jobs"`
+}
+
+type Permissions struct {
+	Checks       string `yaml:"checks"`
+	Contents     string `yaml:"contents,omitempty"`
+	PullRequests string `yaml:"pull-requests,omitempty"`
+	Statuses     string `yaml:"statuses"`
+}
+
+type GenerateOn struct {
+	WorkflowDispatch WorkflowDispatch `yaml:"workflow_dispatch"`
+	Schedule         []Schedule       `yaml:"schedule,omitempty"`
+}
+
+type Jobs struct {
+	Generate Job `yaml:"generate,omitempty"`
+	Publish  Job `yaml:"publish,omitempty"`
+}
+
+type Job struct {
+	Uses    string            `yaml:"uses"`
+	With    map[string]any    `yaml:"with"`
+	Secrets map[string]string `yaml:"secrets"`
+}
+
+type WorkflowDispatch struct {
+	Inputs Inputs `yaml:"inputs"`
+}
+
+type Schedule struct {
+	Cron string `yaml:"cron"`
+}
+
+type Inputs struct {
+	Force Force `yaml:"force"`
+}
+
+type Force struct {
+	Description string `yaml:"description"`
+	Type        string `yaml:"type"`
+	Default     bool   `yaml:"default"`
+}
+
 type SdkGenConfig struct {
 	SdkGenLanguageConfig map[string][]SdkGenConfigField `json:"language_configs"`
-	SdkGenCommonConfig   []SdkGenConfigField            `json:"generation_config"`
+	SdkGenCommonConfig   []SdkGenConfigField            `json:"common_config"`
 }
 
 func GetDefaultConfig(getLangDefaultFunc GetLanguageDefaultFunc, langs ...string) (*Config, error) {
 	cfg := &Config{
 		ConfigVersion: Version,
 		Generation: Generation{
-			SDKClassName:   "SDK",
-			SingleTagPerOp: false,
+			Fields: map[string]any{
+				"sdkClassName":   "SDK",
+				"singleTagPerOp": false,
+			},
 		},
 		Languages: map[string]LanguageConfig{},
 	}
