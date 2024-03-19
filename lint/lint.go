0f634ecebb91d13af6151902f302a2e8c30e8543
Tristan Cartledge <tristan@speakeasyapi.dev>
2024-03-19 13:29:33 -0700

    feat: add support for lint configuration

---
+++ lint/lint.go
@@ -0,0 +1,107 @@
+package lint
+
+import (
+	"errors"
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+
+	"github.com/daveshanley/vacuum/model"
+	"gopkg.in/yaml.v3"
+)
+
+var ErrNotFound = errors.New("could not find lint.yaml")
+
+const (
+	LintVersion = "1.0.0"
+)
+
+const (
+	speakeasyFolder = ".speakeasy"
+	genFolder       = ".gen"
+)
+
+type Ruleset struct {
+	Rulesets []string               `yaml:"rulesets"`
+	Rules    map[string]*model.Rule `yaml:"rules"`
+}
+
+type Lint struct {
+	Version        string             `yaml:"lintVersion"`
+	DefaultRuleset string             `yaml:"defaultRuleset"`
+	Rulesets       map[string]Ruleset `yaml:"rulesets"`
+}
+
+func Load(dir string) (*Lint, string, error) {
+	data, path, err := findValidationFile(dir, "")
+	if err != nil {
+		if !errors.Is(err, ErrNotFound) {
+			return nil, "", err
+		}
+		return nil, "", fmt.Errorf("%w in %s", err, filepath.Join(dir, speakeasyFolder, "lint.yaml"))
+	}
+
+	type lintHeader struct {
+		Version string `yaml:"lintVersion"`
+	}
+
+	var header lintHeader
+	if err := yaml.Unmarshal(data, &header); err != nil {
+		return nil, "", fmt.Errorf("failed to unmarshal lint.yaml: %w", err)
+	}
+
+	if header.Version != LintVersion {
+		return nil, "", fmt.Errorf("unsupported lint version: %s", header.Version)
+	}
+
+	var validation Lint
+	if err := yaml.Unmarshal(data, &validation); err != nil {
+		return nil, "", fmt.Errorf("failed to unmarshal lint.yaml: %w", err)
+	}
+
+	return &validation, path, nil
+}
+
+func findValidationFile(dir, configDir string) ([]byte, string, error) {
+	if configDir == "" {
+		configDir = speakeasyFolder
+	}
+
+	absPath, err := filepath.Abs(dir)
+	if err != nil {
+		return nil, "", fmt.Errorf("failed to get absolute path: %w", err)
+	}
+
+	path := filepath.Join(absPath, configDir, "lint.yaml")
+
+	for {
+		data, err := os.ReadFile(path)
+		if err != nil {
+			if errors.Is(err, fs.ErrNotExist) {
+				// Get the parent directory of the .speakeasy dir
+				currentDir := filepath.Dir(filepath.Dir(path))
+
+				// Check for the root of the filesystem or path
+				// ie `.` for `./something`
+				// or `/` for `/some/absolute/path` in linux
+				// or `:\\` for `C:\\` in windows
+				if currentDir == "." || currentDir == "/" || currentDir[1:] == ":\\" {
+					if configDir == speakeasyFolder {
+						return findValidationFile(dir, genFolder)
+					}
+
+					return nil, "", ErrNotFound
+				}
+
+				// Get the parent directory of the current dir and append ".speakeasy" as we only check in side the .speakeasy dir
+				path = filepath.Join(filepath.Dir(currentDir), configDir, "lint.yaml")
+				continue
+			}
+
+			return nil, "", fmt.Errorf("could not read lint.yaml: %w", err)
+		}
+
+		return data, path, nil
+	}
+}
