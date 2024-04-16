fe7f36146113afb80a5b47f5cec704572db842b6
Robert Crumbaugh <chasecrumbaugh4@gmail.com>
2024-04-16 13:15:23 -0700

    feat: add load/save utilities

--- workflow/lockfile.go
+++ workflow/lockfile.go
@@ -1,5 +1,17 @@
 package workflow
 
+import (
+	"errors"
+	"fmt"
+	"github.com/speakeasy-api/sdk-gen-config/workspace"
+	"gopkg.in/yaml.v3"
+	"io/fs"
+	"os"
+	"path/filepath"
+)
+
+const workflowLockfile = "workflow.lock"
+
 type LockFile struct {
 	SpeakeasyVersion string                `yaml:"speakeasyVersion"`
 	Sources          map[string]SourceLock `yaml:"sources"`
@@ -17,4 +29,52 @@ type SourceLock struct {
 type TargetLock struct {
 	Source          string `yaml:"source,omitempty"`
 	GenLockLocation string `yaml:"genLockLocation,omitempty"`
+}
+
+func LoadLockfile(dir string) (*LockFile, error) {
+	res, err := workspace.FindWorkspace(dir, workspace.FindWorkspaceOptions{
+		FindFile:  workflowLockfile,
+		Recursive: true,
+	})
+	if err != nil {
+		if !errors.Is(err, fs.ErrNotExist) {
+			return nil, err
+		}
+		return nil, fmt.Errorf("%w in %s", err, filepath.Join(dir, workspace.SpeakeasyFolder, workflowLockfile))
+	}
+
+	var lockfile LockFile
+	if err := yaml.Unmarshal(res.Data, &lockfile); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal workflow.lock: %w", err)
+	}
+
+	return &lockfile, nil
+}
+
+// Save the workflow lockfile to the given directory, dir should generally be the root of the project,
+// and the lockfile will be saved to ${projectRoot}/.speakeasy/workflow.lock
+func SaveLockfile(dir string, lockfile *LockFile) error {
+	data, err := yaml.Marshal(lockfile)
+	if err != nil {
+		return fmt.Errorf("failed to marshal workflow lockfile: %w", err)
+	}
+
+	res, err := workspace.FindWorkspace(dir, workspace.FindWorkspaceOptions{
+		FindFile:  workflowLockfile,
+		Recursive: true,
+	})
+	if err != nil {
+		if !errors.Is(err, fs.ErrNotExist) {
+			return err
+		}
+		res = &workspace.FindWorkspaceResult{
+			Path: filepath.Join(dir, workspace.SpeakeasyFolder, workflowLockfile),
+		}
+	}
+
+	if err := os.WriteFile(res.Path, data, 0o644); err != nil {
+		return fmt.Errorf("failed to write workflow.lock: %w", err)
+	}
+
+	return nil
 }
