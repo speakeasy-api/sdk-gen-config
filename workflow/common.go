dff037577c26a62b6f55d30006f979d1eaa061ad
Robert Crumbaugh <chasecrumbaugh4@gmail.com>
2024-06-03 14:17:09 -0700

    fix: remove error type check

--- workflow/common.go
+++ workflow/common.go
@@ -1,7 +1,6 @@
 package workflow
 
 import (
-	"errors"
 	"net/url"
 	"os"
 	"path/filepath"
@@ -21,8 +20,8 @@ func GetFileStatus(filePath string) FileStatus {
 	if strings.Contains(filePath, "registry.speakeasyapi.dev") {
 		return fileStatusRegistry
 	}
-	if _, err := os.Stat(SanitizeFilePath(filePath)); err == nil || !errors.Is(err, os.ErrNotExist) {
-		println("STAT ERROR: ", err.Error())
+
+	if file, err := os.Stat(SanitizeFilePath(filePath)); err == nil || file.Size() > 0 {
 		return fileStatusLocal
 	}
 
