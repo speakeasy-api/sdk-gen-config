312f6098efa23f14d32a9022dab5eb25e0f2e54f
Tristan Cartledge <108070248+TristanSpeakEasy@users.noreply.github.com>
2026-02-10 08:36:06 +1000

    feat: support simplified lint configuration (#114)
    * feat: support simplified lint configuration
    
    * custom rules

--- lint/lint_test.go
+++ lint/lint_test.go
@@ -3,7 +3,9 @@ package lint_test
 import (
 	"os"
 	"path/filepath"
+	"regexp"
 	"testing"
+	"time"
 
 	"github.com/speakeasy-api/sdk-gen-config/lint"
 	"github.com/stretchr/testify/assert"
@@ -41,10 +43,142 @@ rulesets:
 				Rulesets: map[string]lint.Ruleset{
 					"default": {
 						Rulesets: []string{"test"},
-						Rules: map[string]*lint.Rule{
-							"test": {},
+						Rules: []lint.Rule{
+							{
+								ID: "test",
+							},
 						},
 					},
+				},
+			},
+		},
+		{
+			name: "loads v2 lint file",
+			args: args{
+				lintLocation: "test/.speakeasy",
+				lintContents: `lintVersion: 2.0.0
+defaultRuleset: base
+rulesets:
+  base:
+   rulesets:
+     - security
+   rules:
+     security:
+       disabled: true
+`,
+				workingDir: "test",
+			},
+			want: &lint.Lint{
+				Version:        "2.0.0",
+				DefaultRuleset: "base",
+				Rulesets: map[string]lint.Ruleset{
+					"base": {
+						Rulesets: []string{"security"},
+						Rules: []lint.Rule{
+							{
+								ID:       "security",
+								Disabled: true,
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "loads a lint file with regex rule",
+			args: args{
+				lintLocation: "test/.speakeasy",
+				lintContents: `lintVersion: 2.0.0
+defaultRuleset: regexRuleset
+rulesets:
+  regexRuleset:
+    rules:
+      - id: regexRule
+        match: "^test-.*$"
+`,
+				workingDir: "test",
+			},
+			want: &lint.Lint{
+				Version:        "2.0.0",
+				DefaultRuleset: "regexRuleset",
+				Rulesets: map[string]lint.Ruleset{
+					"regexRuleset": {
+						Rules: []lint.Rule{
+							{
+								ID:    "regexRule",
+								Match: regexp.MustCompile("^test-.*$"),
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "loads a lint file with custom rules",
+			args: args{
+				lintLocation: "test/.speakeasy",
+				lintContents: `lintVersion: 2.0.0
+defaultRuleset: default
+rulesets:
+  default:
+    rules:
+      test: {}
+customRules:
+  paths:
+    - "./rules/*.ts"
+`,
+				workingDir: "test",
+			},
+			want: &lint.Lint{
+				Version:        "2.0.0",
+				DefaultRuleset: "default",
+				Rulesets: map[string]lint.Ruleset{
+					"default": {
+						Rules: []lint.Rule{
+							{
+								ID: "test",
+							},
+						},
+					},
+				},
+				CustomRules: &lint.CustomRulesConfig{
+					Paths: []string{"./rules/*.ts"},
+				},
+			},
+		},
+		{
+			name: "loads a lint file with custom rules and timeout",
+			args: args{
+				lintLocation: "test/.speakeasy",
+				lintContents: `lintVersion: 2.0.0
+defaultRuleset: default
+rulesets:
+  default:
+    rules:
+      test: {}
+customRules:
+  paths:
+    - "./rules/*.ts"
+    - "./rules/*.js"
+  timeout: 60s
+`,
+				workingDir: "test",
+			},
+			want: &lint.Lint{
+				Version:        "2.0.0",
+				DefaultRuleset: "default",
+				Rulesets: map[string]lint.Ruleset{
+					"default": {
+						Rules: []lint.Rule{
+							{
+								ID: "test",
+							},
+						},
+					},
+				},
+				CustomRules: &lint.CustomRulesConfig{
+					Paths:   []string{"./rules/*.ts", "./rules/*.js"},
+					Timeout: 60 * time.Second,
 				},
 			},
 		},
@@ -63,6 +197,50 @@ rulesets:
 
 			assert.Equal(t, tt.want, workflowFile)
 			assert.Contains(t, workflowPath, filepath.Join(tt.args.lintLocation, "lint.yaml"))
+		})
+	}
+}
+
+func TestLint_Load_Error(t *testing.T) {
+	type args struct {
+		lintLocation string
+		lintContents string
+		workingDir   string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr string
+	}{
+		{
+			name: "fails to load a lint file with invalid regex rule",
+			args: args{
+				lintLocation: "test/.speakeasy",
+				lintContents: `lintVersion: 2.0.0
+defaultRuleset: regexRuleset
+rulesets:
+  regexRuleset:
+    rules:
+      regexRule:
+        match: "[a-z"
+`,
+				workingDir: "test",
+			},
+			wantErr: "failed to unmarshal lint.yaml: error parsing regexp: missing closing ]: `[a-z`",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			basePath, err := os.MkdirTemp("", "lint*")
+			require.NoError(t, err)
+			defer os.RemoveAll(basePath)
+
+			err = createTempFile(filepath.Join(basePath, tt.args.lintLocation), "lint.yaml", tt.args.lintContents)
+			require.NoError(t, err)
+
+			_, _, err = lint.Load([]string{filepath.Join(basePath, tt.args.workingDir)})
+			require.Error(t, err)
+			assert.EqualError(t, err, tt.wantErr)
 		})
 	}
 }
